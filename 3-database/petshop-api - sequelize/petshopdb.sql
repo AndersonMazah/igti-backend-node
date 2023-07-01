73739a3175ed9c13fee1dafea45c8cc7a68a9f4a
Anderson Martins <anderson.mazah@outlook.com>
2023-07-01 15:19:09 -0300

    feat: add petshop-api (sequelize)

--- 3-database/petshop-api - sequelize/petshopdb.sql
+++ 3-database/petshop-api - sequelize/petshopdb.sql
@@ -4,7 +4,7 @@ CREATE TABLE proprietarios (
 	proprietario_id SERIAL PRIMARY KEY,
 	nome VARCHAR NOT NULL,
 	telefone VARCHAR NOT NULL
-)
+);
 
 CREATE TABLE animais (
 	animal_id SERIAL PRIMARY KEY,
@@ -12,4 +12,12 @@ CREATE TABLE animais (
 	tipo VARCHAR NOT NULL,
 	proprietario_id INT NOT NULL,
 	CONSTRAINT fk_proprietarios FOREIGN KEY (proprietario_id) REFERENCES proprietarios (proprietario_id)
-)
+);
+
+CREATE TABLE servicos (
+	servico_id SERIAL PRIMARY KEY,
+	descricao VARCHAR NOT NULL,
+	valor NUMERIC NOT NULL,
+	animal_id INT NOT NULL,
+	CONSTRAINT fk_animais FOREIGN KEY (animal_id) REFERENCES animais (animal_id)
+);
