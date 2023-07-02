67690e44846a7d18fbdd0d0d1e8ea0ac78a38641
Anderson Martins <anderson.mazah@outlook.com>
2023-07-01 21:03:21 -0300

    add exemplos de teste com jest

---
+++ 4-testes/teste-com-jest-mock/src/exemplo-mock.js
@@ -0,0 +1,18 @@
+function realizarParaMaiorDeIdade(pessoas, callback) {
+  for (let i = 0; i < pessoas.length; i += 1) {
+    if (pessoas[i].idade >= 18) {
+      callback(pessoas[i]);
+    }
+  }
+}
+
+function aguardarTimer(callback) {
+  setTimeout(() => {
+    callback();
+  }, 3000);
+}
+
+module.exports = {
+  realizarParaMaiorDeIdade,
+  aguardarTimer,
+};
