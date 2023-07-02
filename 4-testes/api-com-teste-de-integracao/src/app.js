a6c5c89018c46836dcf02798d3837a4b2939f19d
Anderson Martins <anderson.mazah@outlook.com>
2023-07-02 15:59:19 -0300

    feat: add api com testes de integração (incompleto)

---
+++ 4-testes/api-com-teste-de-integracao/src/app.js
@@ -0,0 +1,10 @@
+// import express from "express";
+
+// const app = express();
+// app.use(express.json());
+
+// app.get('/', async (req, res) => {
+//     res.status(200).send('Bootcamp desenvolvedor back end - Tópicos especiais!');
+// });
+
+// export default { app }
